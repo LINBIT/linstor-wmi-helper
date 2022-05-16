7b16d9345930bdd3ac17bdab6466b3e2c90d97d0
Johannes Thoma <johannes@johannesthoma.com>
2022-05-16 17:39:35 +0200

    Cleaned up a little ...

--- linstor-helper.cs
+++ linstor-helper.cs
@@ -1,65 +1,31 @@
 using System;
 using System.Management;
 
-/*
-	string NamespacePath = "\\\\.\\ROOT\\Microsoft\\Windows\\Storage\\providers_v2";
-	string ClassName = "MSFT_StorageSubSystem";
-	
-	//Create ManagementClass
-	ManagementClass mClass = new ManagementClass(NamespacePath + ":" + ClassName);
-	
-	// List the properties in the MSFT_StorageSubSystem class
-	PropertyDataCollection lproperties = mClass.Properties;
-	
-	// display the properties  
-	Console.WriteLine(string.Format("Property Names in {0}: ",ClassName));
-	foreach (PropertyData property in lproperties)
-	{
-	    Console.WriteLine("name: {0}, Origin: {1}", property.Name, property.Origin );
-	}	
-*/
 class Program
 {
 	private static ManagementObjectCollection GetStoragePools()
 	{
 		ManagementClass m = new ManagementClass("\\\\.\\ROOT\\Microsoft\\Windows\\Storage:MSFT_StoragePool");
-	// 	ManagementClass m = new ManagementClass("\\\\.\\ROOT\\Microsoft\\Windows\\Storage\\providers_v2:MSFT_StorageSubSystem");
-		// ManagementClass m = new ManagementClass("CIM_Service");
-		// ManagementClass m = new ManagementClass("MSFT_StoragePool");
-		EnumerationOptions e = new EnumerationOptions();
-		e.EnumerateDeep = true;
-		Console.Write("X1");
-		// var p = m.GetMethodParameters("GetSupportedSize");
-		var p = m.GetMethodParameters("GetSupportedSize");
-		/*
-		ManagementBaseObject params = m.GetMethodParameters("GetSupportedSize");
-		*/
+		ManagementBaseObject p = m.GetMethodParameters("GetSupportedSize");
 		p["ResiliencySettingName"] = "Simple";
 
-		foreach (ManagementObject o in m.GetInstances(e))
+		foreach (ManagementObject o in m.GetInstances())
 		{
 			Console.WriteLine("Instance "+o["FriendlyName"]);
 			ManagementBaseObject r = o.InvokeMethod("GetSupportedSize", p, null);
-//			UInt64[] Sizes = r["SupportedSizes"];
-			// String status = r["ExtendedStatus"];
 
-//			Assert.IsNotNull(r);
-			// Console.WriteLine("Max Size "+r["VirtualDiskSizeMax"].ToString());
 			Console.WriteLine("Max Size "+r["VirtualDiskSizeMax"]);
 			Console.WriteLine("Min Size "+r["VirtualDiskSizeMin"]);
 			Console.WriteLine("Divisor "+r["VirtualDiskSizeDivisor"]);
-			// Console.WriteLine("Sizes "+Sizes.length);
 			Console.WriteLine("retval "+r["ReturnValue"]);
 			Console.WriteLine("status "+r["ExtendedStatus"]);
 			Console.WriteLine("supported sizes "+r["SupportedSizes"]);
 		}
-		Console.Write("X2");
 		return m.GetInstances();
 	}
 			
 	public static void Main(string[] args)
 	{
-		Console.Write("Hello World!");
 		GetStoragePools();
 	}
 }
