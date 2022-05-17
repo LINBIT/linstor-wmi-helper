23a55f2b3a7a3e437d18416c8bb212aeacba4621
Johannes Thoma <johannes@johannesthoma.com>
2022-05-17 18:07:53 +0200

    Attempt to get MSFT_Disk from MSFT_VirtualDisk

--- linstor-helper.cs
+++ linstor-helper.cs
@@ -23,6 +23,32 @@ class Program
 		return arr[0];
 	}
 
+	private static ManagementObject GetDiskForVirtualDisk(ManagementBaseObject vdisk)
+	{
+		// var query = new ManagementObjectSearcher("ROOT\\Microsoft\\Windows\\Storage", "Associators Of {MSFT_VirtualDisk.ObjectID="+vdisk["ObjectID"]+"}");
+		// var query = new ManagementObjectSearcher("ROOT\\Microsoft\\Windows\\Storage", "Associators Of {MSFT_VirtualDisk} where ClassDefsOnly");
+		// var query_string = "Associators Of {MSFT_VirtualDisk.ObjectID='"+vdisk["ObjectID"]+"'} where ClassDefsOnly";
+		// var query_string = "Associators Of {MSFT_VirtualDisk.ObjectID='"+vdisk["ObjectID"]+"'}";
+		// var query_string = "Associators Of {MSFT_VirtualDisk.ObjectID='{1}\\\\\\\\SERVER2019-1\\\\root/Microsoft/Windows/Storage/Providers_v2\\SPACES_VirtualDisk.ObjectId=\\\"{33fba0cb-bf8f-11ec-9b04-806e6f6e6963}:VD:{3143d14e-abf2-4bba-bc1d-7cf19ae0ba48}{0b274417-075e-4864-8d1f-ae605634c414}\\\"'}";
+		// var query_string = "Associators Of {MSFT_VirtualDisk.ObjectID='%7b1%7d%5c%5cSERVER2019-1%5croot/Microsoft/Windows/Storage/Providers_v2%5c%5cSPACES_VirtualDisk.ObjectId=%22%7b33fba0cb-bf8f-11ec-9b04-806e6f6e6963%7d:VD:%7b3143d14e-abf2-4bba-bc1d-7cf19ae0ba48%7d%7b0b274417-075e-4864-8d1f-ae605634c414%7d%22'}";
+		// var query_string = "Associators Of {MSFT_VirtualDisk.ObjectID='%7b1%7d'}"; /* works */
+		// var query_string = "Associators Of {MSFT_VirtualDisk.ObjectId='"+Uri.EscapeUriString(vdisk["ObjectID"].ToString())+"'}";
+		var query_string = "Associators Of {MSFT_VirtualDisk.ObjectID='"+Uri.EscapeUriString(vdisk["ObjectID"].ToString())+"'} where ClassDefsOnly"; /* not found */
+//		var query_string = "Associators Of {MSFT_VirtualDisk.ObjectId="+Uri.EscapeUriString(vdisk["ObjectID"].ToString())+"} where ClassDefsOnly";
+
+Console.WriteLine(query_string);
+		
+		var query = new ManagementObjectSearcher("ROOT\\Microsoft\\Windows\\Storage", query_string);
+		var res = query.Get();
+		ManagementObject[] arr = { null };
+
+		if (res.Count != 1) {
+			throw new Exception("Expected Disk object for Virtual Disk with object ID "+vdisk["ObjectID"]+" to exist and be unique, I got "+res.Count+" objects.");
+		}
+		res.CopyTo(arr, 0);
+		return arr[0];
+	}
+
 	private static void InitializeWMIClasses()
 	{
 		StoragePoolClass = new ManagementClass("\\\\.\\ROOT\\Microsoft\\Windows\\Storage:MSFT_StoragePool");
@@ -48,9 +74,10 @@ class Program
 		Console.WriteLine("status "+ret["ExtendedStatus"]);
 		Console.WriteLine("virtualdisk "+ret["CreatedVirtualDisk"]);
 
-		ManagementBaseObject disk = (ManagementBaseObject) ret["CreatedVirtualDisk"];
-		if (disk != null) {
-			Console.WriteLine("class is "+disk.ClassPath);
+		ManagementBaseObject vdisk = (ManagementBaseObject) ret["CreatedVirtualDisk"];
+		if (vdisk != null) {
+			Console.WriteLine("class is "+vdisk.ClassPath);
+			ManagementObject disk = GetDiskForVirtualDisk(vdisk);
 		}
 
 		return null;
