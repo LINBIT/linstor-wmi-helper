d101c1f38c48e85aa68f0854db16e9917c7e25c0
Johannes Thoma <johannes@johannesthoma.com>
2022-05-17 14:02:27 +0200

    Working on virtual-disk create command.
    Command line args should be similar to linstor client.

--- linstor-helper.cs
+++ linstor-helper.cs
@@ -28,6 +28,12 @@ class Program
 		StoragePoolClass = new ManagementClass("\\\\.\\ROOT\\Microsoft\\Windows\\Storage:MSFT_StoragePool");
 	}
 
+	private static ManagementObject CreateVirtualDisk(ManagementObject pool, string friendly_name, ulong size)
+	{
+		Console.Write("About to create virtual disk "+friendly_name+" with "+size+" bytes");
+		return null;
+	}
+
 	public static void Main(string[] args)
 	{
 		InitializeWMIClasses();
@@ -37,6 +43,14 @@ class Program
 			Console.WriteLine("Usage: linstor-helper <storage-pool-friendly-name>");
 			return;
 		}
+		if (args[0] == "virtual-disk") {
+			if (args[1] == "create") {
+				var the_pool = GetStoragePoolByFriendlyName(args[2]);
+				var disk = CreateVirtualDisk(the_pool, args[3], ulong.Parse(args[4]));
+				return;
+			}
+		}
+
 		var pool = GetStoragePoolByFriendlyName(args[0]);
 
 		ManagementBaseObject p = StoragePoolClass.GetMethodParameters("GetSupportedSize");
