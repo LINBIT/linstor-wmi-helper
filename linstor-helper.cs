c9fe50bcda2d46e6b641fa92c070d9d947f19b17
Johannes Thoma <johannes@johannesthoma.com>
2022-05-17 14:35:45 +0200

    Can create virtual disks

--- linstor-helper.cs
+++ linstor-helper.cs
@@ -28,9 +28,26 @@ class Program
 		StoragePoolClass = new ManagementClass("\\\\.\\ROOT\\Microsoft\\Windows\\Storage:MSFT_StoragePool");
 	}
 
-	private static ManagementObject CreateVirtualDisk(ManagementObject pool, string friendly_name, ulong size)
+		/* This calls the WMI method CreateVirtualDisk of the 
+		 * MSFT_StoragePool class (pool parameter) in order to
+		 * create a virtual disk on this storage pool.
+		 */
+
+	private static ManagementObject CreateVirtualDisk(ManagementObject pool, string friendly_name, ulong size, bool thin)
 	{
-		Console.Write("About to create virtual disk "+friendly_name+" with "+size+" bytes");
+		Console.Write("About to create virtual disk "+friendly_name+" with "+size+" bytes\n");
+		ManagementBaseObject p = StoragePoolClass.GetMethodParameters("CreateVirtualDisk");
+		p["FriendlyName"] = friendly_name;
+		p["Size"] = size;
+		p["ProvisioningType"] = thin ? 1 : 2;
+		p["ResiliencySettingName"] = "Simple"; /* no RAID for now */
+		p["Usage"] = 1;
+		p["OtherUsageDescription"] = "WinDRBD backing disk";
+		ManagementBaseObject ret = pool.InvokeMethod("CreateVirtualDisk", p, null);
+		Console.WriteLine("retval "+ret["ReturnValue"]);
+		Console.WriteLine("status "+ret["ExtendedStatus"]);
+		Console.WriteLine("virtualdisk "+ret["CreatedVirtualDisk"]);
+
 		return null;
 	}
 
@@ -46,7 +63,7 @@ class Program
 		if (args[0] == "virtual-disk") {
 			if (args[1] == "create") {
 				var the_pool = GetStoragePoolByFriendlyName(args[2]);
-				var disk = CreateVirtualDisk(the_pool, args[3], ulong.Parse(args[4]));
+				var disk = CreateVirtualDisk(the_pool, args[3], ulong.Parse(args[4]), args[5] == "thin");
 				return;
 			}
 		}
